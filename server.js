4de7d549f28a88962eca7a882fddb45465467600
shanejohnson <shane.john316@gmail.com>
2024-02-13 18:41:05 -0600

    Added logic to make existing notes show up on the left hand side

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname,'/public/notes.html'))
 })
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error reading notes');
+            return;
+        }
+
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 })
